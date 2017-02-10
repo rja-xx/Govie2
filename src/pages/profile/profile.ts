62ccbbb6fa3a588e21233e83dcf7c4db5fd78ad6
rogerjakobsen <roger.jakobsen@embriq.no>
2017-02-10 09:44:54 +0100

    Corrected login logout logic, logout button is now at profile page

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,22 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import {UserService} from "../../providers/session/UserService";
+import { Events } from 'ionic-angular';
 
 /*
-  Generated class for the Profile page.
+ Generated class for the Profile page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
+ See http://ionicframework.com/docs/v2/components/#navigation for more info on
+ Ionic pages and navigation.
+ */
 @Component({
-  selector: 'page-profile',
-  templateUrl: 'profile.html'
+    selector: 'page-profile',
+    templateUrl: 'profile.html',
+    providers: [UserService]
 })
 export class ProfilePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+    constructor(public navCtrl:NavController, public navParams:NavParams, public userService:UserService, public events:Events) {
+    }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ProfilePage');
-  }
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad ProfilePage');
+    }
+
+    logout() {
+        this.userService.logout(this.events);
+    }
 
 }
--- src/providers/session/UserService.ts
+++ src/providers/session/UserService.ts
@@ -5,29 +5,37 @@ import {User} from "../../model/user";
 @Injectable()
 export class UserService {
 
-  private twitterProvider:any;
-  public user:User;
+    private twitterProvider:any;
+    public user:User;
 
-  init() {
-    this.twitterProvider = new firebase.auth.TwitterAuthProvider();
-  }
+    init() {
+        this.twitterProvider = new firebase.auth.TwitterAuthProvider();
+    }
 
-  login(events) {
-    return firebase.auth().signInWithPopup(this.twitterProvider).then(function (authData) {
-      events.publish("user:login", new User(authData.user.uid, authData.user.photoURL, authData.user.displayName));
-    }).catch(function (error) {
-      console.log(error);
-      events.publish("error:login");
-    });
-  }
+    login(events) {
+        if (this.currentUser() === null) {
+            return firebase.auth().signInWithPopup(this.twitterProvider).then(function (authData) {
+                events.publish("user:login", new User(authData.user.uid, authData.user.photoURL, authData.user.displayName));
+            }).catch(function (error) {
+                console.log(error);
+                events.publish("error:login");
+            });
+        }
+    }
 
-  logout(events) {
-    return firebase.auth().signOut().then(function () {
-      events.publish("user:logout");
-    }, function (error) {
-      console.log(error);
-      events.publish("error:logout");
-    });
-  }
+    logout(events) {
+        if (this.currentUser()) {
+            return firebase.auth().signOut().then(function () {
+                events.publish("user:logout");
+            }, function (error) {
+                console.log(error);
+                events.publish("error:logout");
+            });
+        }
+    }
+
+    currentUser() {
+        return firebase.auth().currentUser;
+    }
 }
 
