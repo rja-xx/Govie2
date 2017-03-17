139743bf9be9f408fdc2088608561e34d675709a
rogerjakobsen <roger.jakobsen@embriq.no>
2017-03-17 16:17:38 +0100

    Landed the process of retreiving profile from rebase properly

--- client/src/pages/profile/profile.ts
+++ client/src/pages/profile/profile.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef} from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Events } from 'ionic-angular';
 import { ModalController } from 'ionic-angular';
@@ -13,12 +13,17 @@ import {Profile} from "../../model/profile";
 })
 export class ProfilePage {
 
-    private profile:Profile;
+    private profile:Profile = new Profile({});
 
-    constructor(public navCtrl:NavController, public navParams:NavParams, public userService:UserService, public modalCtrl:ModalController) {
-        debugger;
+    constructor(public navCtrl:NavController,
+                public navParams:NavParams,
+                public userService:UserService,
+                public modalCtrl:ModalController,
+                cd:ChangeDetectorRef) {
         this.userService.getProfile().then(res => {
+            debugger;
             this.profile = new Profile(res);
+            cd.detectChanges();
         });
     }
 
